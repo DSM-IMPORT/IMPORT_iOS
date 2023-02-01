2716185a2837e0c56ae73be933bc61908d025b66
junha <goodjunha@gmail.com>
2023-02-01 15:39:44 +0900

    ✨:: BaseVC 추가

---
+++ Import/Import/Source/Base/BaseVC.swift
@@ -0,0 +1,43 @@
+//
+//  BaseVC.swift
+//  Import
+//
+//  Created by 박준하 on 2023/01/31.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+import RxRelay
+import RxFlow
+
+import Then
+import SnapKit
+
+import Moya
+import Alamofire
+import Kingfisher
+
+class BaseVC: UIViewController {
+    let bound = UIScreen.main.bounds
+    var disposeBag = DisposeBag()
+    
+    override func viewDidLayoutSubviews() { self.layout() }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+        self.bind()
+        self.configure()
+        self.attribute()
+        self.view.setNeedsUpdateConstraints()
+    }
+    
+    func configure() { }
+    func bind() { }
+    func layout() { }
+    func attribute() { }
+}
+
+
