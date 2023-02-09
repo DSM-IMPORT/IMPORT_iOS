a5a95e113c37b6d512fad1b942ff10d965ef15ff
junha <goodjunha@gmail.com>
2023-02-09 17:12:26 +0900

    🍀:: 첫번째 회원가입 완성

---
+++ Import/Import/Source/ViewControllers/Auth/SignUp/SignUpFirstViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+import SnapKit
+import RxFlow
+import RxSwift
+import RxCocoa
+
+class SignUpFirstViewController: BaseSV {
+    
+    var initialStep: Step {
+        IMPORTStep.homeIsRequired
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateWith(self)
+    }
+    
+    override func updateWith(_ controller: UIViewController) {
+        super.updateWith(controller)
+        
+        firstTitle.text = "새로운 계정으로 시작해요"
+        secondTitle.text = "전화번호 인증을 통해 시작해 보세요"
+        
+        firstTextField.placeholder = "전화번호를 입력해 주세요"
+        secondTextField.placeholder = "전송된 인증번호를 입력해 주세요"
+        
+    }
+    
+    override func attribute() {
+        self.view.backgroundColor = .systemBackground
+    }
+    
+    func signupButtonDidTap() {
+        self.steps.accept(IMPORTStep.homeIsRequired)
+    }
+}
