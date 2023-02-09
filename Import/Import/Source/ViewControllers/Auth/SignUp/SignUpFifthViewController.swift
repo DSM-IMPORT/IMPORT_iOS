2cc934f41243afb475cad5e9cefddb981b6686b2
junha <goodjunha@gmail.com>
2023-02-09 17:52:24 +0900

    🍀:: 비밀번호 입력 뷰 완성

---
+++ Import/Import/Source/ViewControllers/Auth/SignUp/SignUpFifthViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpFifthViewController.swift
+//  Import
+//
+//  Created by 박준하 on 2023/02/09.
+//
+
+import UIKit
+
+class SignUpFifthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //여기가 귀찮은데 분야 설정 페이지
+    }
+}
---
+++ Import/Import/Source/ViewControllers/Auth/SignUp/SignUpfourthViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignUpfourthViewController.swift
+//  Import
+//
+//  Created by 박준하 on 2023/02/09.
+//
+
+import UIKit
+import SnapKit
+import RxFlow
+import RxSwift
+import RxCocoa
+
+class SignUpfourthViewController: BaseSV {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateWith(self)
+    }
+    
+    override func updateWith(_ controller: UIViewController) {
+        super.updateWith(controller)
+        
+        firstTitle.text = "새로운 계정으로 시작해요"
+        secondTitle.text = "비밀번호를 입력하여 시작해 보세요"
+        
+        firstTextField.placeholder = "비밀번호를 입력해 주세요"
+        secondTextField.placeholder = "비밀번호를 한 번 더 입력해 주세요"
+        
+    }
+    
+    override func attribute() {
+        self.view.backgroundColor = .systemBackground
+    }
+    
+    func signupButtonDidTap() {
+        self.steps.accept(IMPORTStep.homeIsRequired)
+    }
+}
