3bf11e9a181a868984e56a56369aea84b9c394e1
junha <goodjunha@gmail.com>
2023-02-28 18:53:20 +0900

    ⚡️ :: 둥근 Tapbar 완성

--- Import/Import/Source/TabBar/TabBarViewController.swift
+++ Import/Import/Source/TabBar/TabBarViewController.swift
@@ -6,46 +6,67 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
-//class TapBarViewController : UITabBarController {
-//
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animrated)
-//
-//        let homeVC = UINavigationController(rootViewController: HomeViewController())
-//        let tabOneBarItem = UITabBarItem(title: "",
-//                                         image: UIImage(systemName: "house"),
-//                                         tag: 1)
-//
-//        homeVC.tabBarItem = tabOneBarItem
-//
-//        let chartVC = UINavigationController(rootViewController: MassgeViewController())
-//        let tabTwoBarItem2 = UITabBarItem(title: "",
-//                                          image: UIImage(systemName: "chart.bar.fill"),
-//                                          tag: 2)
-//
-//        chartVC.tabBarItem = tabTwoBarItem2
-//
-//        let searchVC = UINavigationController(rootViewController: PlusViewController())
-//        let tabThrBarItem3 = UITabBarItem(title: "",
-//                                          image: UIImage(systemName: "magnifyingglass"),
-//                                          tag: 3)
-//
-//        searchVC.tabBarItem = tabThrBarItem3
-//
-//
-//        let userVC = UINavigationController(rootViewController: UserViewController())
-//        let tabFiveBarItem4 = UITabBarItem(title: "",
-//                                           image: UIImage(systemName: "person"),
-//                                           tag: 5)
-//        userVC.tabBarItem = tabFiveBarItem4
-//
-//        self.tabBar.tintColor = UIColor(named: "errorColor")
-//        self.tabBar.unselectedItemTintColor = .gray
-//        self.viewControllers = [homeVC,
-//                                chartVC,
-//                                searchVC,
-//                                userVC]
-//        self.tabBar.backgroundColor = UIColor(named: "CollectionViewColor")
-//    }
-//}
+@available(iOS 16.0, *)
+class TapBarViewController: UITabBarController {
+    private lazy var homeViewController: UINavigationController = {
+        let viewController = HomeViewController()
+        let tabBarItem = UITabBarItem(title: "도원준", image: UIImage(systemName: "house.fill"), tag: 0)
+        viewController.tabBarItem = tabBarItem
+        let navigationView = UINavigationController(rootViewController: viewController)
+        tabBar.tintColor = .red
+        return navigationView
+    }()
+    
+    private lazy var chattingViewController: UIViewController = {
+        let viewController = ChattingViewController()
+        let tabBarItem = UITabBarItem(title: "비밀", image: UIImage(systemName: "lock.fill"), tag: 1)
+        viewController.tabBarItem = tabBarItem
+        let navigationView = UINavigationController(rootViewController: viewController)
+        return navigationView
+    }()
+    
+    private lazy var recruitmentViewController: UIViewController = {
+        let viewController = RecruitmentViewController()
+        let tabBarItem = UITabBarItem(title: "사진", image: UIImage(systemName: "photo.fill"), tag: 2)
+        viewController.tabBarItem = tabBarItem
+        let navigationView = UINavigationController(rootViewController: viewController)
+        return navigationView
+    }()
+    
+    private lazy var userViewController: UIViewController = {
+        let viewController = UserViewController()
+        let tabBarItem = UITabBarItem(title: "유저", image: UIImage(systemName: "photo.fill"), tag: 3)
+        viewController.tabBarItem = tabBarItem
+        let navigationView = UINavigationController(rootViewController: viewController)
+        return navigationView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers = [homeViewController, chattingViewController, recruitmentViewController, userViewController]
+        configureTabBar()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        var tabFrame = self.tabBar.frame
+        tabFrame.size.height = 90
+        tabFrame.origin.y = self.view.frame.size.height - 90
+        self.tabBar.frame = tabFrame
+    }
+}
+
+extension UITabBarController {
+    
+    func configureTabBar() {
+        self.tabBar.tintColor = .label
+        self.tabBar.layer.cornerRadius = 20
+        self.tabBar.layer.masksToBounds = true
+        self.tabBar.layer.backgroundColor = UIColor.systemBackground.cgColor
+        //        self.tabBar.layer.borderWidth = 0.4
+    }
+}
