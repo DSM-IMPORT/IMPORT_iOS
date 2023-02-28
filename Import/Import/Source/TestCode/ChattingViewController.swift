794b0214a9ace1fe0ac5b459cb22e7f29db0967b
junha <goodjunha@gmail.com>
2023-02-28 18:53:57 +0900

    🧪:: 탭바 안에 들어갈 임시 메소드 지정

---
+++ Import/Import/Source/TestCode/ChattingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChattingViewController.swift
+//  Import
+//
+//  Created by 박준하 on 2023/02/28.
+//
+
+import UIKit
+
+class ChattingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
---
+++ Import/Import/Source/TestCode/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  Import
+//
+//  Created by 박준하 on 2023/02/28.
+//
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
